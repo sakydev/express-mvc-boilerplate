b89d223cee755d90e0a0c1e3ac411290f6cfff3a
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-03-20 08:41:11 +0100

    use status code lib

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,11 +1,12 @@
 const postService = require('../services/PostService')
+const statusCode = require('http-status-codes');
 
 const show = async (request, response) => {
     const postId = parseInt(request.params.postId);
     try {
         const post = await postService.show(postId);
         if (!post) {
-            response.status(404).send();
+            response.status(statusCode.NOT_FOUND).send();
         } else {
             response.send({
                 "content": post
@@ -14,20 +15,20 @@ const show = async (request, response) => {
     } catch (error) {
         console.error('Error fetching post:', error);
 
-        response.status(500).send({ error: 'Internal Server Error' });
+        response.status(statusCode.INTERNAL_SERVER_ERROR).send({ error: 'Internal Server Error' });
     }
 }
 
 const store = async (request, response) => {
     try {
         const createdPost = await postService.create(request.body);
-        response.status(201).send({
+        response.status(statusCode.CREATED).send({
             "content": createdPost
         });
     } catch (error) {
         console.error('Error creating post:', error);
 
-        response.status(500).send({ error: 'Internal Server Error' });
+        response.status(statusCode.INTERNAL_SERVER_ERROR).send({ error: 'Internal Server Error' });
     }
 }
 
